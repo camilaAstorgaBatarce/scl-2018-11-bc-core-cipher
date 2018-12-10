17d67845266baf816c5197648c60bd08259c95fc
camilaAstorgaBatarce <camilaastorga03@gmail.com>
2018-12-10 20:24:04 -0300

    pasa los espacios, las ñ, Ñ, y simbolos, todo bien hasta el offset 33 (faltan los offset negativos)

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,46 @@
 window.cipher = {
-  encode: (frase, offset) => {
+  encode: (texto, offset) => {
     /* Acá va tu código */
     var result = '';
-    var texto = frase.toUpperCase();
-
     offset = parseInt(offset);
     for (var i = 0; i < texto.length; i++) {
       if (texto[i] == ' '){
         result += ' ';
-    } else{
+      } else if (texto[i] == "ñ") {
+          result += "ñ";
+        } else if (texto[i] == "Ñ") {
+          result += "Ñ";
+        continue;
+    } else {
       var posicion_ascii = texto.charCodeAt(i);
-      var nueva_posicion_ascii = (posicion_ascii - 65 + offset) % 26 + 65;
+      if (posicion_ascii>= 33 && posicion_ascii<=126){
+      var nueva_posicion_ascii = (posicion_ascii - 33 + offset) % 93 + 33;
       result += String.fromCharCode(nueva_posicion_ascii);
+    }
       }
     }
     return result;
   },
-  decode: (frase, offset) => {
+  decode: (texto, offset) => {
     /* Acá va tu código */
   var result_decodificar = '';
-  var texto = frase.toUpperCase();
   offset = parseInt(offset);
     for (var i = 0; i < texto.length; i++) {
         if (texto[i] == ' '){
           result_decodificar += ' ';
-      } else{
+        } else if (texto[i] == "ñ") {
+            result_decodificar += "ñ";
+          } else if (texto[i] == "Ñ") {
+            result_decodificar += "Ñ";
+          continue;
+      } else {
       var posicion_ascii = texto.charCodeAt(i);
-      var nueva_posicion_ascii = (posicion_ascii - 90 - offset) % 26 + 90;
+      if (posicion_ascii>= 33 && posicion_ascii<=126){
+      var nueva_posicion_ascii = (posicion_ascii - 126 - offset) % 93 + 126;
       result_decodificar += String.fromCharCode(nueva_posicion_ascii)
     }
     }
+  }
     return result_decodificar;
   }
 };
